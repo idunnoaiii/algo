d021d839790dcaeea09089c2aaa08c10153aefb4
Thien.Nguyen <idunno.aiii@gmail.com>
2023-05-15 22:24:51 +0700

    add triplet closet to target sum

--- pattern/2/5_mine.js
+++ pattern/2/5_mine.js
@@ -1,13 +1,11 @@
 
 function solve(arr = [], target) {
+
     arr = arr.sort((a, b) => a - b)
 
     let result = Number.MAX_VALUE
-
+    
     for (let i = 0; i < arr.length; i++) {
-
-        if (i < arr.length - 1 && arr[i] == arr[i + 1])
-            continue
 
         let subTargetSum = target - arr[i]
 
@@ -17,7 +15,7 @@ function solve(arr = [], target) {
 
     }
 
-    return result
+    return target - result
 
 }
 
@@ -26,27 +24,25 @@ function closetTwoSum(arr = [], target) {
 
     let start = 0
     let end = arr.length - 1
-
     let result = Number.MAX_VALUE
 
     while (start < end) {
-
         let tempSum = arr[start] + arr[end]
-
-        if (tempSum == target) {
+        
+        if (target == tempSum) 
             return target
-        } else if (tempSum < target) {
-            start += 1
-        } else {
-            end -= 1
-        }
+        
+        result = Math.min(result, Math.abs(tempSum - target))
 
-        result = Math.min(result, Math.abs(target - tempSum))
+        if (tempSum < target) 
+            start += 1
+        else 
+            end -= 1
     }
 
-    return result
+    return target - result
 }
 
-//console.log(solve([-2, 0, 1, 2], 2))
+console.log(solve([-2, 0, 1, 2], 2))
 console.log(solve([-3, -1, 1, 2], 1))
 console.log(solve([1, 0, 1, 1], 100))
