05e80929ea3c3c2751fd63833e3985737639e7c2
idunno.aiii@gmail.com <idunno.aiii@gmail.com>
2024-02-26 07:05:09 +0700

    findStringAnagrams | done

--- grokking/1_sliding/code.js
+++ grokking/1_sliding/code.js
@@ -11,41 +11,41 @@
  * @returns {boolean}
  */
 const findPermutation = function (arr, pattern) {
-  if (arr.length === 0 || pattern.length === 0) return true;
+	if (arr.length === 0 || pattern.length === 0) return true;
 
-  let frequenceChar = new Map();
-  let isMatch = 0;
-  let wStart = 0;
+	let frequenceChar = new Map();
+	let isMatch = 0;
+	let wStart = 0;
 
-  for (let char of pattern) {
-    frequenceChar.set(char, (frequenceChar.get(char) || 0) + 1);
-  }
+	for (let char of pattern) {
+		frequenceChar.set(char, (frequenceChar.get(char) || 0) + 1);
+	}
 
-  for (let wEnd = 0; wEnd < arr.length; wEnd++) {
-    let curChar = arr[wEnd];
+	for (let wEnd = 0; wEnd < arr.length; wEnd++) {
+		let curChar = arr[wEnd];
 
-    if (frequenceChar.has(curChar)) {
-      frequenceChar.set(curChar, frequenceChar.get(curChar) - 1);
-      if (frequenceChar.get(curChar) === 0) {
-        isMatch += 1;
-      }
-    }
+		if (frequenceChar.has(curChar)) {
+			frequenceChar.set(curChar, frequenceChar.get(curChar) - 1);
+			if (frequenceChar.get(curChar) === 0) {
+				isMatch += 1;
+			}
+		}
 
-    if (isMatch === frequenceChar.size) return true;
+		if (isMatch === frequenceChar.size) return true;
 
-    if (wEnd - wStart + 1 >= pattern.length) {
-      let curStartChar = arr[wStart];
-      if (frequenceChar.has(curStartChar)) {
-        if (frequenceChar.get(curStartChar) === 0) {
-          isMatch -= 1;
-        }
-        frequenceChar.set(curStartChar, frequenceChar.get(curStartChar) + 1);
-      }
-      wStart += 1;
-    }
-  }
+		if (wEnd - wStart + 1 >= pattern.length) {
+			let curStartChar = arr[wStart];
+			if (frequenceChar.has(curStartChar)) {
+				if (frequenceChar.get(curStartChar) === 0) {
+					isMatch -= 1;
+				}
+				frequenceChar.set(curStartChar, frequenceChar.get(curStartChar) + 1);
+			}
+			wStart += 1;
+		}
+	}
 
-  return false;
+	return false;
 };
 
 //console.log(findPermutation("oidbcaf", "abc")); //true, The string contains "bca" which is a permutation of the given pattern.
@@ -68,39 +68,40 @@ const findPermutation = function (arr, pattern) {
  * @returns {number[]}
  */
 const findStringAnagrams = function (arr, pattern) {
-  let posIndex = [];
-  let basket = new Map();
+	let posIndex = []
+	let basket = new Map()
 
-  for (let item of pattern) {
-    basket.set(item, (basket.get(item) || 0) + 1);
-  }
+	for (let item of pattern) {
+		basket.set(item, (basket.get(item) || 0) + 1)
+	}
 
-  let start = 0;
+	let start = 0
 
-  for (let end = 0; end < arr.length; end++) {
-    let curChar = arr[end];
-    if (basket.get(curChar)) {
-      basket.set(curChar, basket.get(curChar) - 1);
-      if (basket.get(curChar) === 0) {
-        basket.delete(curChar);
-      }
-    }
+	for (let end = 0; end < arr.length; end++) {
+		let curEndChar = arr[end]
 
-    if (basket.size === 0) posIndex.push(start);
+		basket.set(curEndChar, (basket.get(curEndChar) || 0) - 1)
+		if (basket.get(curEndChar) === 0)
+			basket.delete(curEndChar)
 
-    if (end - start + 1 >= pattern.length) {
-      let startCurChar = arr[start];
-      basket.set(startCurChar, (basket.get(startCurChar) || 0) + 1);
-      start += 1;
-      startCurChar = arr[start];
-      basket.set(startCurChar, (basket.get(startCurChar) || 0) - 1);
-      if (basket.get(startCurChar) === 0) {
-        basket.delete(startCurChar);
-      }
-    }
-  }
+		if (end - start + 1 >= pattern.length) {
+			if (basket.size === 0)
+				posIndex.push(start)
 
-  return posIndex;
+			let startChar = arr[start]
+
+			basket.set(startChar, (basket.get(startChar) || 0) + 1)
+
+			if (basket.get(startChar) === 0)
+				basket.delete(startChar)
+
+
+			start += 1
+
+		}
+	}
+
+	return posIndex;
 };
 
 console.log(findStringAnagrams("ppqp", "pq")); //[1,2], The two anagrams of the pattern in the given string are "pq" and "qp".
